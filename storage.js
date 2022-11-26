36d688c050ec2899cb7fd22bb0fefd7417ca0edf
Femi Laizer <femilaizer@gmail.com>
2022-11-26 18:21:15 +0100

    started the express server on port 5000

---
+++ storage.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        firstname: "Oluwafemi",
+        lastname: "Laizer",
+        birthday: "04/08/1986",
+        id: 01
+    },
+    {
+        firstname: "Akorede",
+        lastname: "Laizer",
+        birthday: "08/04/2021",
+        id: 02
+    },
+    {
+        firstname: "Oluronke",
+        lastname: "Laizer",
+        birthday: "19/02/1995",
+        id: 03
+    },
+];
+
+module.exports = { members };
