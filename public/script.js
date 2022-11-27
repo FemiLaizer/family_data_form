a1df45fd67b7a9280a5d7cde313d1e5dccd6b495
Femi Laizer <femilaizer@gmail.com>
2022-11-27 02:09:03 +0100

    updated html with input

--- public/script.js
+++ public/script.js
@@ -1,2 +1,12 @@
 /**/
 console.log("working");
+
+// const members = require('../storage');
+
+confirmBtn = document.getElementById('confirm_btn');
+
+const checkMemberInData = () => {
+    console.log({ members })
+}
+
+confirmBtn.addEventListener('click', checkMemberInData);
